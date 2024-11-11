5e1ea283cd0a07ad719cf89ca228381763606cb0
the_aceix <aceixsmartx@gmail.com>
2024-11-11 15:09:19 +0000

    CAR-139: change the email and form on the website
    - Rename template references from "Titanium" to "CareFusion365"
    - Correct the waitlist form link

--- js/forms.js
+++ js/forms.js
@@ -3,7 +3,7 @@ document.getElementById('waitlistForm').addEventListener('submit', function (eve
     var name = document.getElementById('name').value;
     var email = document.getElementById('email').value;
 
-    const formAction = `https://docs.google.com/forms/d/e/1FAIpQLSdj9IUfhc0IhzZh13-N1kzNGL8ME1iYOoL_cLnCTps0P5T_MA/viewform?usp=pp_url&entry.715831605=${email}&entry.801251707=${name}`
+    const formAction = `https://docs.google.com/forms/d/e/1FAIpQLSfUwTkV8s08inHwzWzqhkupaP17dsMQK_vxhx2yYOvP_Wqxbw/viewform?usp=pp_url&entry.2130784172=${email}&entry.2005620554=${name}`
 
     window.open(formAction, '_blank');
 
