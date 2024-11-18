171f9ecc03d694b1e50ddc1c9d884067ca006f3b
Elorm Benedict Lokpo <157585048+ElormLokpo@users.noreply.github.com>
2024-11-18 11:40:32 +0000

    CAR-136 Design organisation details form (#3)
    * CAR-136 Design organisation details form
    
    * CAR-136 API requests
    
    * CAR-136 re-working organization request body structure
    
    * CAR-136 removed auth redirect function

--- pages/admin-auth/js/main.js
+++ pages/admin-auth/js/main.js
@@ -10,51 +10,84 @@ function showToast(type, message) {
 }
 
 
-document.getElementById("auth-register-form").addEventListener("submit", async (event) => {
-    event.preventDefault();
+let authRegisterFormElement = document.getElementById("admin-auth-register-form")
 
-    let firstname = document.getElementById("firstname").value;
-    let lastname = document.getElementById("lastname").value;
-    let email = document.getElementById("email").value;
-    let mobile = document.getElementById("mobile").value;
-    let password = document.getElementById("password").value;
-
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+if (authRegisterFormElement) {
+    authRegisterFormElement.addEventListener("submit", async (event) => {
+        event.preventDefault();
 
 
-    if (!firstname || !lastname || !email || !mobile || !password) {
-        document.getElementById("firstname").classList.add("input-error")
+        let firstname = document.getElementById("firstname").value;
+        let username = document.getElementById("username").value;
+        let lastname = document.getElementById("lastname").value;
+        let dob = document.getElementById("dob").value;
 
-        document.getElementById("lastname").classList.add("input-error")
+        let email = document.getElementById("email").value;
+        let password = document.getElementById("password").value;
 
-
-        document.getElementById("email").classList.add("input-error")
-
-
-        document.getElementById("password").classList.add("input-error")
-
-
-        document.getElementById("mobile").classList.add("input-error")
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
 
-        document.getElementById("error-text").hidden = false
+        if (!firstname || !lastname || !email || !password) {
+            document.getElementById("firstname").classList.add("input-error")
+            document.getElementById("lastname").classList.add("input-error")
+            document.getElementById("email").classList.add("input-error")
+            document.getElementById("password").classList.add("input-error")
 
-        showToast("error", "All fields are required")
-    }
-    else if (!emailRegex.test(email)) {
-        document.getElementById("email").classList.add("input-error");
-        document.getElementById("email-label").classList.add("label-error");
+            document.getElementById("error-text").hidden = false
 
-    } else {
-        showToast("success", "Admin account created successfully")
-        window.location.href = "/pages/organization/add-organization.html"
-        //axios functionality;
-        // await axios.post("", {firstname, lastname, email, mobile, password})
-        // .then(response=>{
-        //     console.log(response)
-        // })
-    }
+            showToast("error", "All fields are required")
+        }
+        else if (!emailRegex.test(email)) {
+            document.getElementById("email").classList.add("input-error");
+            document.getElementById("email-label").classList.add("label-error");
+
+        } else {
+            sessionStorage.setItem("authData", JSON.stringify({ first_name: firstname, last_name: lastname, username, email, password,date_of_birth:dob }));
+            showToast("success", "Admin account created successfully")
+            window.location.href = "/pages/organization/add-organization.html"
+
+        }
 
 
-    
-})
+
+    })
+}
+
+
+
+let authLoginFormElement = document.getElementById("admin-auth-login-form")
+
+if (authLoginFormElement) {
+    authLoginFormElement.addEventListener("submit", async (event) => {
+        event.preventDefault();
+
+        let email = document.getElementById("email-login").value;
+        let password = document.getElementById("password-login").value;
+
+        if (!email || !password) {
+            document.getElementById("email-login").classList.add("input-error")
+
+            document.getElementById("password-login").classList.add("input-error")
+
+            document.getElementById("error-text-login").hidden = false
+
+            showToast("error", "All fields are required")
+        } else {
+
+            await axios.post("http://localhost:8000/api/admins/auth/login", { email, password })
+                .then((response) => {
+
+                    if (response.status === 200) {
+                        showToast("success", "Login successful");
+                        sessionStorage.setItem("refresh_token", response.data.refresh)
+                        sessionStorage.setItem("access_token", response.data.access)
+
+                        window.location.href = "/pages/organization/organization-review.html"
+
+
+                    }
+                })
+        }
+    })
+}
